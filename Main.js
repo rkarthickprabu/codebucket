8cc90dcecce85227f76d222a82054654adffbc4d
Karthick <rkarthickprabu@gmail.com>
2015-10-03 11:51:51 +1000

    No changes to JS file. Just doing this after configuring git config file on system to add user name and email

--- Main.js
+++ Main.js
@@ -4,4 +4,29 @@ function alltab()
 	alert("Welcome! Keep track and take control!")
 }
 
-alltab();
+//alltab();
+
+prompt("Enter your time in months of when you would like to finish this task")
+
+
+function reminder(month) {
+
+	if(month > 12){
+
+		//years = (month/12);
+
+		var years = Math.floor(month/12);
+
+		var month = month%12;
+
+		alert("You still have " + years + " years and " + month + " months left!");
+	}
+else{
+
+ var month = 12 - month;
+ alert("You still have " + month + " months left!");
+}
+}
+document.getElementById("demo").innerHTML = myFunction(4, 3);
+//return reminder();
+
